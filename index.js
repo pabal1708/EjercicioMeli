2a100723dd7234ce6ab73f54e9bd5f91dcc9f63b
ferdiaz93 <fernandodiaz1209@gmail.com>
2019-05-17 17:04:06 -0300

    actividad practica

--- index.js
+++ index.js
@@ -4,6 +4,9 @@ const cookieParser = require('cookie-parser');
 const logger = require('morgan');
 const express = require("express");
 const app = express();
+//libreria agregada
+const fs = require("fs");
+const mercadopago = require('mercadopago')
 
 const port = 8080;
 
@@ -27,12 +30,34 @@ app.use((err, req, res, next) => {
 });
 
 app.get("/", (req, res, next) => {
-    res.json({
-        message: 'OK',
-    });
+  res.json({
+      message: 'OK',
+  });
 });
 
 app.get("/ping", (req, res, next) => {
+  var payment = {
+    description: 'celular',
+    transaction_amount: 10500,
+    payment_method_id: 'rapipago',
+    payer: {
+      email: 'test_user_3931694@testuser.com',
+      identification: {
+        type: 'DNI',
+        number: '34123123'
+      }
+    }
+  };
+  
+  mercadopago.payment.create(payment, {
+    access_token: 'MY_MERCHANT_ACCESS_TOKEN',
+  }).then(function (mpResponse) {
+    if(payment && acces_token == true){
+      res.send("se hizo la compra")
+    }
+  });
+  
+  
   res.json(["pong"]);
 });
 
